129e59379a10d697df7ca382f8c2388dd4ff693e
SantBreno <brenosantsousa4@outlook.com>
2022-09-29 16:59:14 -0300

    Initial Commit

---
+++ app/src/main/java/com/projetos/roomnote/db/NoteDatabase.kt
@@ -0,0 +1,4 @@
+package com.projetos.roomnote.db
+
+class NoteDatabase {
+}
---
+++ app/src/main/java/com/projetos/roomnote/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.projetos.roomnote.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.projetos.roomnote.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
